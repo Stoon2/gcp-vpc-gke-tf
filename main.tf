57c49ad5c708b530276e0535c513c369aad6dbfe
Stoon <melsayed.se@gmail.com>
2022-05-07 20:44:57 +0200

    complete infrastructure

--- main.tf
+++ main.tf
@@ -5,43 +5,43 @@ provider "google" {
 }
 
 module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 4.0"
+  source  = "terraform-google-modules/network/google"
+  version = "~> 4.0"
 
-    project_id   = "aqueous-freedom-348421"
-    network_name = "final-task-vpc"
-    routing_mode = "REGIONAL"
+  project_id   = "aqueous-freedom-348421"
+  network_name = "final-task-vpc"
+  routing_mode = "REGIONAL"
 
-    # delete_default_internet_gateway_routes = true
+  # delete_default_internet_gateway_routes = true
 
-    subnets = [
-        {
-            subnet_name           = "public-sn"
-            subnet_ip             = "10.0.0.0/24"
-            subnet_region         = "us-central1"
-            subnet_private_access = "false"
-            subnet_flow_logs      = "false"
-            description           = "This subnet is public"
-        },
-        {
-            subnet_name           = "private-sn"
-            subnet_ip             = "10.0.1.0/24"
-            subnet_region         = "us-central1"
-            subnet_private_access = "true"
-            subnet_flow_logs      = "false"
-            description           = "This subnet is private"
-        }
-    ]
+  subnets = [
+    {
+      subnet_name           = "public-sn"
+      subnet_ip             = "10.0.0.0/24"
+      subnet_region         = "us-central1"
+      subnet_private_access = "false"
+      subnet_flow_logs      = "false"
+      description           = "This subnet is public"
+    },
+    {
+      subnet_name           = "private-sn"
+      subnet_ip             = "10.0.1.0/24"
+      subnet_region         = "us-central1"
+      subnet_private_access = "true"
+      subnet_flow_logs      = "false"
+      description           = "This subnet is private"
+    }
+  ]
 
-    routes = [
-        {
-            name              = "egress-internet"
-            description       = "route through IGW to access internet"
-            destination_range = "0.0.0.0/0"
-            tags              = "egress-inet"
-            next_hop_internet = "true"
-        }
-    ]
+  routes = [
+    {
+      name              = "egress-internet"
+      description       = "route through IGW to access internet"
+      destination_range = "0.0.0.0/0"
+      tags              = "egress-inet"
+      next_hop_internet = "true"
+    }
+  ]
 }
 
 module "cloud_router" {
@@ -52,14 +52,18 @@ module "cloud_router" {
   project = "aqueous-freedom-348421"
   region  = "us-central1"
   network = module.vpc.network_name
-  nats    = [
+  nats = [
     {
       name                               = "nat-gateway"
       nat_ip_allocate_option             = "AUTO_ONLY"
       source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-      subnetworks                        = [
+      subnetworks = [
         {
           name                    = "private-sn"
+          source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+        },
+        {
+          name                    = "public-sn"
           source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
         }
       ]
@@ -70,7 +74,6 @@ module "cloud_router" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-public-instance"
   machine_type = "e2-micro"
-#   tags = ["ssh-rule-final"]
 
   boot_disk {
     initialize_params {
@@ -80,8 +83,8 @@ resource "google_compute_instance" "vm_instance" {
 
   network_interface {
     # A default network is created for all GCP projects
-    network = module.vpc.network_name
-    subnetwork  = "public-sn"
+    network    = module.vpc.network_name
+    subnetwork = "public-sn"
     # Remove public IP by hashing 'access_config'
     # access_config {
     # }
@@ -91,6 +94,10 @@ resource "google_compute_instance" "vm_instance" {
     email  = google_service_account.final-cluster-admin.email
     scopes = ["cloud-platform"]
   }
+
+  depends_on = [
+    module.vpc
+  ]
 }
 
 # firewall resource
@@ -103,12 +110,16 @@ resource "google_compute_firewall" "ssh-rule-final" {
     ports    = ["22"]
   }
 
-  source_ranges = ["35.235.240.0/20"]
+  source_ranges = ["0.0.0.0/0"]
+
+  depends_on = [
+    module.vpc
+  ]
 }
 
 # first service account & bindings
 resource "google_service_account" "final-cluster-admin" {
-  account_id = "cluster-admin"
+  account_id   = "cluster-admin"
   display_name = "final-k8s-admin"
 }
 resource "google_project_iam_member" "final-admin-binding" {
@@ -119,7 +130,7 @@ resource "google_project_iam_member" "final-admin-binding" {
 
 # second service account & bindings
 resource "google_service_account" "final-node-accounts" {
-  account_id = "node-acc"
+  account_id   = "node-acc"
   display_name = "node-acc"
 }
 resource "google_project_iam_member" "final-node-binding" {
@@ -129,9 +140,9 @@ resource "google_project_iam_member" "final-node-binding" {
 }
 
 # Moi un clusters
- resource "google_container_cluster" "cluster" {
-  name     = "final-cluster"
-  location = "us-central1"
+resource "google_container_cluster" "cluster" {
+  name                     = "final-cluster"
+  location                 = "us-central1"
   remove_default_node_pool = true
   initial_node_count       = 1
   network                  = "final-task-vpc"
@@ -141,21 +152,25 @@ resource "google_project_iam_member" "final-node-binding" {
     "us-central1-b"
   ]
   master_authorized_networks_config {
-    cidr_blocks{
-        cidr_block = "10.0.0.0/24"
-        display_name = "managment-cidr"
+    cidr_blocks {
+      cidr_block   = "10.0.0.0/24"
+      display_name = "managment-cidr"
     }
   }
-  
+
   ip_allocation_policy {
 
   }
-  
+
   private_cluster_config {
     enable_private_nodes    = true
     enable_private_endpoint = true
     master_ipv4_cidr_block  = "192.168.1.0/28"
   }
+
+  depends_on = [
+    module.vpc
+  ]
 }
 
 resource "google_container_node_pool" "nodePool" {
@@ -166,13 +181,16 @@ resource "google_container_node_pool" "nodePool" {
 
   node_config {
     preemptible  = true
-    machine_type = "e2-micro"
+    machine_type = "e2-medium"
 
     service_account = google_service_account.final-node-accounts.email
-    oauth_scopes    = [
+    oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
+  depends_on = [
+    module.vpc
+  ]
 }
 
 # resource "google_compute_instance" "vm_private_instance" {
